a9c216f69f5a13081bb8521848047a013d0fe139
agate <agate.hao@gmail.com>
2015-08-05 11:02:24 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'octokit', '~> 3.8'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'highline'
+gem 'multi_json'
+gem 'oj'
---
+++ server.rb
@@ -0,0 +1,76 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'octokit'
+require 'oj'
+require 'multi_json'
+require 'highline/import'
+require 'yaml'
+
+def ask_access_token(prompt="Enter Your Access Token")
+  ask(prompt) { |q| q.echo = false }
+end
+
+CONFIG = YAML.load_file(File.expand_path('../config/app.yml', __FILE__))
+CLIENT = Octokit::Client.new \
+  access_token: ask_access_token,
+  auto_paginate: true
+
+get '/' do
+  MultiJson.dump({
+    status: "ok",
+    response: {
+      endpoints: [
+        '/repos/:owner/:repo/issues/:id'
+      ]
+    }
+  })
+end
+
+get '/repos/:owner/:repo/issues/:id' do
+  repo = "#{params[:owner]}/#{params[:repo]}"
+
+  unless CONFIG['valid_repos'].key?(params[:owner]) and
+         CONFIG['valid_repos'][params[:owner]].include?(params[:repo])
+
+    status 403
+    MultiJson.dump({
+      status: "error",
+      message: "We don't support '#{repo}' yet."
+    })
+
+  else
+    begin
+      res = CLIENT.issue(repo, params[:id])
+      MultiJson.dump({
+        status: "ok",
+        response: res
+      })
+    rescue Octokit::NotFound => e
+      MultiJson.dump({
+        status: "error",
+        message: "can not find issue(#{params[:id]}) under #{repo}."
+      })
+    rescue Octokit::Unauthorized => e
+      MultiJson.dump({
+        status: "error",
+        message: "github unauthorized. please contact @honghao!"
+      })
+    rescue Exception => e
+      MultiJson.dump({
+        status: "error",
+        message: "unknown exception"
+      })
+
+      puts [
+        Time.now.to_s,
+        "ERROR",
+        "/repos/#{params[:owner]}/#{params[:repo]}/issues/#{params[:id]}",
+        MultiJson.dump(params),
+        e.inspect
+      ].join("\t")
+    end
+  end
+end
